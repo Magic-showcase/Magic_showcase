a938656f829cd3f4dee6796691521f3b5dd236fb
Yosmar2020 <ryosmar53@gmail.com>
2020-10-26 09:03:09 -0700

    Mostrar_Blog

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from Blog.models import POST
 # Create your views here.
 
 def Blog(request):
-    return render(request,'Centro/Blog.html')
+
+    post = POST.objects.all()
+
+    return render(request,'Centro/Blog.html',{"post":post})
