5e6f10b60d375d33b99df7a659401309c283f0bc
Yosmar2020 <ryosmar53@gmail.com>
2020-11-01 11:24:49 -0700

    Corregir_Modelos_Tienda

--- Tienda/admin.py
+++ Tienda/admin.py
@@ -1,19 +1,35 @@
 from django.contrib import admin
 from .models import Producto
 from .models import Envio
+from .models import Venta_descrip
 # Register your models here.
-
+from .models import Venta
 class Productoadmin(admin.ModelAdmin):
     readonly_fields=('Created','Update')
+    list_display=['Nombre']
 
 class Envioadmin(admin.ModelAdmin):
     model=Envio
-    list_display=['Numero','Fecha','Entregado']
+    list_display=['Fecha','Entregado']
     #Filtrar informacion por campo
     list_filter=('Fecha','Entregado',)
     #detectar meses y años 
     date_hierarchy='Fecha'
 
+class Venta_descrip_admin(admin.ModelAdmin):
+    model=Venta_descrip
+    list_display=['Cliente','Cantidad']
+    list_filter=('Cliente','Producto')
+
+class Venta_admin(admin.ModelAdmin):
+    model=Venta
+    list_display=['Venta_desc','Cliente','Costo_total']
+    list_filter=('Cliente','Costo_total')
+
 admin.site.register(Producto,Productoadmin)
 #Registro de table Envio
 admin.site.register(Envio,Envioadmin)
+#Registo de tabla Venta_descrp
+admin.site.register(Venta_descrip,Venta_descrip_admin)
+#Registro venta
+admin.site.register(Venta,Venta_admin)
--- Tienda/models.py
+++ Tienda/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Producto(models.Model):
     Nombre=models.CharField(max_length=50)
     Seccion=models.CharField(max_length=50)
     Contenido=models.CharField(max_length=50)
-    Precio=models.IntegerField()
+    Precio=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True,)
     #guardar sitio adecuado de imagen upload_to
     Imagen=models.ImageField(upload_to='Tienda')
     Created=models.DateTimeField(auto_now_add=True)
@@ -16,8 +16,29 @@ class Producto(models.Model):
     def _str_(self):
         return self.Nombre
 
+class Venta_descrip(models.Model):
+    Cliente=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    Producto=models.ManyToManyField(Producto)
+    Cantidad=models.IntegerField()
+    Precio_unitario=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    class Meta:
+        verbose_name='Venta_descrip'
+        verbose_name_plural='Venta_descrip'
+
+
+class Venta(models.Model):
+    Venta_desc=models.ForeignKey(Venta_descrip,on_delete=models.DO_NOTHING)
+    Cliente=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    subtotal=models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    Precio_IVA=models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    Costo_total=models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    class Meta:
+       verbose_name='Venta'
+       verbose_name_plural='Ventas'
+
 class Envio(models.Model):
-    Numero=models.IntegerField()
+    Cliente=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    Venta=models.ForeignKey(Venta,on_delete=models.DO_NOTHING,default=None)
     Fecha=models.DateField()
     Entregado=models.BooleanField()
     class Meta:
