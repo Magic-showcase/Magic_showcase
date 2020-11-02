290f63357c4bb0e7e0ded092aa53b57605c24c1a
LeoTerrazas <a1116150109@utch.edu.mx>
2020-11-01 20:32:03 -0700

    Modelo

--- Tutorials/models.py
+++ Tutorials/models.py
@@ -3,5 +3,15 @@ from django.db import models
 # importar clase de usuario
 from django.contrib.auth.models import User
 
-# crear clase categoria tutoriales
-# prueba de commit
+# create your models here
+
+class Categoria(models.Model):
+    Nombre=models.CharField(max_length=50)
+    Created=models.DateTimeField(auto_now_add=True)
+    Update=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name='Categoria'
+        verbose_name_plural='Categorias'
+    
+    def _str_(self):
+        return self.Nombre
