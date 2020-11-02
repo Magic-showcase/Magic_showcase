f29654561809a3e3fa85a457c1f02ab6dfaf10a6
Yosmar2020 <ryosmar53@gmail.com>
2020-11-01 19:50:17 -0700

    Blog

--- Blog/models.py
+++ Blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-# Create your models here.
+#Create your models here.
 
 class Categoria(models.Model):
     Nombre=models.CharField(max_length=50)
