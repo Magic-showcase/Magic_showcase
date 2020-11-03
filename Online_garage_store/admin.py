ad41e283ad8391e17c449aca48e22d636727275a
Yosmar2020 <ryosmar53@gmail.com>
2020-11-02 19:06:22 -0700

    Modelo_Online_garage_store

--- Online_garage_store/admin.py
+++ Online_garage_store/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
-
+from .models import User_Product
+from .models import User_Envio
+from .models import User_Venta
+from .models import User_Venta_descrip
+from .models import Vendedor
 # Register your models here.
+class Vendedoradmin(admin.ModelAdmin):
+    model=Vendedor
+    list_display=['Usuario','Sexo','Edad','Email','Domicilio']
+
+class User_Productoadmin(admin.ModelAdmin):
+    readonly_fields=('Created','Update')
+    list_display=['Nombre','Vendedor']
+
+class User_Envioadmin(admin.ModelAdmin):
+    model=User_Envio
+    list_display=['Vendedor','Fecha','Entregado']
+    #Filtrar informacion por campo
+    list_filter=('Fecha','Entregado',)
+    #detectar meses y años 
+    date_hierarchy='Fecha'
+
+class User_Venta_descrip_admin(admin.ModelAdmin):
+    model=User_Venta_descrip
+    list_display=['Vendedor','Cliente','Cantidad','Producto','Precio_unitario']
+    list_filter=('Cliente','Producto')
+
+class User_Venta_admin(admin.ModelAdmin):
+    model=User_Venta
+    list_display=['Vendedor','Venta_desc','Cliente','Costo_total']
+    list_filter=('Cliente','Costo_total')
+#Registro de table vendedor
+admin.site.register(Vendedor,Vendedoradmin)
+#
+admin.site.register(User_Product,User_Productoadmin)
+#Registro de table Envio
+admin.site.register(User_Envio,User_Envioadmin)
+#Registo de tabla Venta_descrp
+admin.site.register(User_Venta_descrip,User_Venta_descrip_admin)
+#Registro venta
+admin.site.register(User_Venta,User_Venta_admin)
--- Online_garage_store/models.py
+++ Online_garage_store/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Vendedor(models.Model):
+    Usuario=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    Foto=models.ImageField(upload_to='Online_garage_store')
+    Edad=models.IntegerField()
+    Sexo=models.CharField(max_length=50)
+    Email=models.EmailField()
+    Domicilio=models.CharField(max_length=70)
 
-# Create your models here.
+class User_Product(models.Model):
+    Vendedor=models.ForeignKey(Vendedor,on_delete=models.DO_NOTHING,default=None)
+    Nombre=models.CharField(max_length=50)
+    Seccion=models.CharField(max_length=50)
+    Contenido=models.CharField(max_length=50)
+    Precio=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True,)
+    #guardar sitio adecuado de imagen upload_to
+    Imagen=models.ImageField(upload_to='Online_garage_store')
+    Created=models.DateTimeField(auto_now_add=True)
+    Update=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name='Producto_usuario'
+        verbose_name_plural='Productos_usuario'
+    
+    def _str_(self):
+        return self.Nombre
+
+class User_Venta_descrip(models.Model):
+    Vendedor=models.ForeignKey(Vendedor,on_delete=models.DO_NOTHING,default=None)
+    Cliente=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    Producto=models.ForeignKey(User_Product,on_delete=models.DO_NOTHING,default=None)
+    Cantidad=models.IntegerField()
+    Precio_unitario=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    class Meta:
+        verbose_name='Usuario_Venta_descrip'
+        verbose_name_plural='Usuario_Venta_descrip'
+
+
+class User_Venta(models.Model):
+    Venta_desc=models.ForeignKey(User_Venta_descrip,on_delete=models.DO_NOTHING)
+    Vendedor=models.ForeignKey(Vendedor,on_delete=models.DO_NOTHING,default=None)
+    Cliente=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    subtotal=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    Precio_IVA=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    Costo_total=models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    class Meta:
+       verbose_name='Venta_usuario'
+       verbose_name_plural='Ventas_usuario'
+
+class User_Envio(models.Model):
+    Vendedor=models.ForeignKey(Vendedor,on_delete=models.DO_NOTHING,default=None)
+    Cliente=models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
+    Venta=models.ForeignKey(User_Venta,on_delete=models.DO_NOTHING,default=None)
+    Address=models.TextField()
+    Fecha=models.DateField()
+    Entregado=models.BooleanField()
+    class Meta:
+        verbose_name='Envio_usuario'
+        verbose_name_plural='Envios_usuario'
