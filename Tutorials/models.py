7cbe234bea3888679b3ccea5fe653f33f5f82465
LeoTerrazas <a1116150109@utch.edu.mx>
2020-11-04 10:57:19 -0700

    Mostrar modelo en pagina

--- Tutorials/models.py
+++ Tutorials/models.py
@@ -15,3 +15,22 @@ class Categoria(models.Model):
     
     def _str_(self):
         return self.Nombre
+
+
+class Tutoriales(models.Model):
+    Titulo=models.CharField(max_length=50)
+    Contenido=models.TextField()
+    #guardar sitio adecuado de imagen upload_to
+    Imagen=models.ImageField(upload_to='Blog', null=True, blank=True)
+    #si un autor se va se borra el post
+    Autor=models.ForeignKey(User,on_delete=models.CASCADE)
+    #relacion entre post y categoria
+    Categorias=models.ManyToManyField(Categoria)
+    Created=models.DateTimeField(auto_now_add=True)
+    Update=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name='Tutorial'
+        verbose_name_plural='Tutoriales'
+    
+    def _str_(self):
+        return self.Titulo
