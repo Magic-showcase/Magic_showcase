6d05551c8c9f546e62d5db7e4aa3ea3579038ca2
Yosmar2020 <ryosmar53@gmail.com>
2020-11-09 20:48:36 -0700

    intento_form

--- Blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-class FormularioBlog(forms.Form):
-
-    Titulo=forms.CharField()
-    Contenido=forms.CharField()
-    Imagen=forms.ImageField()
-    Autor=forms.CharField()
-    Categorias=forms.CharField()
--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from Blog.models import POST
+from Blog.models import POST,Categoria
 # Create your views here.
-from Blog.forms import FormularioBlog
+from django.contrib.auth.models import User
 def Blog(request):
 
     post = POST.objects.all()
@@ -12,12 +12,11 @@ def CreateBog(request):
     if request.method=="POST":
         Ti=request.POST["Titulo"]
         Con=request.POST["Contenido"]
-        Aut=request.POST["Autor"]
-        Cate=request.POST["Categorias"]
-
-
-
-        return render(request,"Centro/gracias.html")
+        Cat= Categoria.objects.get(Nombre="Terror")
+        Autor= User.objects.get(first_name="Rene")
+        POST5 = POST.objects.create(Titulo="Prueba",Contenido="adsdasdasdasd",Autor=Autor,Categorias=Cat)
+        POST5.save()
+        #return render(request,"Centro/gracias.html")
 
 
     return render(request,"Centro/Crate_blog.html")
