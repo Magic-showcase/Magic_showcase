ba83e7ffe7406c74d7d4e35ede5332e7bb6fc47b
Yosmar2020 <ryosmar53@gmail.com>
2020-11-10 17:40:48 -0700

    Validar_login

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from Blog.models import POST,Categoria
 from Usuario.models import Users
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def Blog(request):
             
@@ -8,12 +9,13 @@ def Blog(request):
 
     return render(request,'Centro/Blog.html',{"post":post})
 
+@login_required
 def CreateBog(request):
     if request.method=="POST":
         Ti=request.POST["Titulo"]
         Con=request.POST["Contenido"]
         Cat= Categoria.objects.get(Nombre="Terror")
-        Autor= Users.objects.get(user="Rene")
+        Autor= Users.user.objects.get(first_name="Rene")
         POST5 = POST.objects.create(Titulo="Prueba",Contenido="adsdasdasdasd",Autor=Autor,Categorias=Cat)
         POST5.save()
         #return render(request,"Centro/gracias.html")
