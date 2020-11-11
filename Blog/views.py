0b085e2ae198317e8ef85a5c8793bddc4f8af0ba
Yosmar2020 <ryosmar53@gmail.com>
2020-11-10 20:26:56 -0700

    Modi

--- Blog/views.py
+++ Blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from Blog.models import POST,Categoria
 from Usuario.models import Users
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def Blog(request):
             
@@ -14,9 +15,11 @@ def CreateBog(request):
     if request.method=="POST":
         Ti=request.POST["Titulo"]
         Con=request.POST["Contenido"]
-        Cat= Categoria.objects.get(Nombre="Terror")
+        #Cat = Categoria.objects.get(id=1)
+        #Cate = get_object_or_404(Categoria, id=4)
+        Cate = Categoria.objects.filter(id='1')
         Autor= Users.objects.get(user_id=1)
-        POST5 = POST.objects.create(Titulo="Prueba",Contenido="adsdasdasdasd",Autor=Autor,Categorias=Cat)
+        POST5 = POST.objects.create(Titulo="Prueba",Contenido="adsdasdasdasd",Autor=Autor,Categorias=Cate)
         POST5.save()
         #return render(request,"Centro/gracias.html")
 
