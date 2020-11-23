61b4483a427149952cb9db637e1637829a1a718b
LeoTerrazas <a1116150109@utch.edu.mx>
2020-11-22 17:40:24 -0700

    URLS Tutorialsv2

--- Tutorialsv2/views.py
+++ Tutorialsv2/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Tutoriales
 # Create your views here.
+
+def Tutorial(request):
+
+    tutorial = Tutoriales.objects.all()
+
+    return render(request,'Centro/Tutoriales.html',{"tutorial":tutorial})
