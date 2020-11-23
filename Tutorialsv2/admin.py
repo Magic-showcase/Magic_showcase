7fa18415a1a2d2476ab98f3300127ec7cc5dc652
LeoTerrazas <a1116150109@utch.edu.mx>
2020-11-22 17:37:08 -0700

    Admin Tutorialsv2

--- Tutorialsv2/admin.py
+++ Tutorialsv2/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Categoria, Tutoriales
 
 # Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):
+    model=Categoria
+    readonly_fields=('Created','Update')
+    list_display=['Nombre']
+    search_fields=('Nombre',)
+
+
+class TutorialAdmin(admin.ModelAdmin):
+    model=Tutoriales
+    readonly_fields=('Created','Update')
+    list_display=['Titulo','Autor']
+    search_fields=('Titulo','Autor')
+    list_filter=('Titulo','Autor','Categorias',)
+
+admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Tutoriales,TutorialAdmin)
