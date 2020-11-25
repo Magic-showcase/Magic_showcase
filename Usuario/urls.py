bad78325ac9e522beabb8e97e5ca4bf26eb1c4dd
Yosmar2020 <ryosmar53@gmail.com>
2020-11-24 23:38:55 -0700

    cambiar_password

--- Usuario/urls.py
+++ Usuario/urls.py
@@ -4,10 +4,31 @@ from . import views
 from django.conf import settings
 #importar archivos static
 from django.conf.urls.static import static
+#django reset
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('Login/',views.Logins,name="Login"),
     path('Logout/',views.Logouts_,name="Logout"),
     path('Register/',views.Regi,name="Register"),
     path('Perfil/',views.modi,name="Perfil"),
+    #Restaurar pasword
+     path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='Centro/Restaurar_password/password_change.html'),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='Centro/Restaurar_password/password_change_done.html'),
+         name='password_change_done'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='Centro/Restaurar_password/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='Centro/Restaurar_password/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name='Centro/Restaurar_password/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='Centro/Restaurar_password/password_reset_complete.html'),
+         name='password_reset_complete')
+    
 ]
