5cdb163245ea43f18b1dae8f429bc0d7b3940fe2
Yosmar2020 <ryosmar53@gmail.com>
2020-11-26 20:17:28 -0700

    static2

--- Integradora/settings.py
+++ Integradora/settings.py
@@ -159,7 +159,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'Nucleo/static',
+    BASE_DIR / 'static',
 )
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
