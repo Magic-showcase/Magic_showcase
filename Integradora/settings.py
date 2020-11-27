259d07465a08a6bd6c0f735f0021d2ab07a1a767
Yosmar2020 <ryosmar53@gmail.com>
2020-11-26 20:21:15 -0700

    ststica

--- Integradora/settings.py
+++ Integradora/settings.py
@@ -157,12 +157,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
+
 
 
 
